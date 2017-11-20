ce1402bb010fd42d29954f3a665bf92e8620a976
Thierry Chappuis <thierry@chappuis.io>
2017-11-20 18:49:45 +0100

    Mise à jour de publishconf.py

--- publishconf.py
+++ publishconf.py
@@ -10,7 +10,7 @@ import sys
 sys.path.append(os.curdir)
 from pelicanconf import *
 
-SITEURL = 'https://openclassmates.github.io'
+SITEURL = 'https://openclassmates.github.io/openclassrooms'
 RELATIVE_URLS = False
 
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
