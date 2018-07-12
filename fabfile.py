f115450ff34edcb187e1e03605a70e8131ba6530
Thierry Chappuis <thierry@chappuis.io>
2018-07-12 09:29:51 +0200

    updated fabfile.py

--- fabfile.py
+++ fabfile.py
@@ -2,9 +2,16 @@ import os
 
 from invoke import task
 
-PELICAN = 'pelican'
+PELICAN = './env/bin/pelican'
 
 @task
+def venv(c):
+    """Creates a virtualenv for the build."""
+    if not os.path.exists('env/bin/pip'):
+        c.run('python3 -m venv env')
+    c.run('./env/bin/pip install -r requirements.txt')
+
+@task(venv)
 def build(c):
     """Builds the Pelican blog with dev configuration."""
     c.run(
@@ -12,7 +19,7 @@ def build(c):
         '--fatal=warnings content'
     )
 
-@task
+@task(venv)
 def publish(c):
     """Builds the Pelican blog with publish configuration."""
     c.run(
