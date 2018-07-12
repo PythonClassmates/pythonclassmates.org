22cb459e3da33b24236650867f5521843ffc01e2
Thierry Chappuis <thierry@chappuis.io>
2018-07-12 08:14:26 +0200

    Added build script using Fabric.

---
+++ fabfile.py
@@ -0,0 +1,27 @@
+from invoke import task
+
+PELICAN = 'pipenv run pelican'
+
+@task
+def build(c):
+    """Builds the Pelican blog with dev configuration."""
+    c.run(
+        f'{PELICAN} -s pelicanconf.py '
+        '--fatal=warnings content'
+    )
+
+@task
+def publish(c):
+    """Builds the Pelican blog with publish configuration."""
+    c.run(
+        f'{PELICAN} -s publishconf.py '
+        '--fatal=warnings content'
+    )
+
+@task
+def revert_commit(c):
+    """Reverts to previous commit."""
+    c.run('git revert HEAD -n')
+    c.run('git commit -m "Revert to last commit because errors were found."')
+    c.run('git checkout -b "errors"')
+    c.run('git push -f https://${GITHUB_TOKEN}@github.com/PythonClassmates/PythonClassmates.org.git errors:master')
