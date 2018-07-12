f954b4568ad746cfb0780bbc8a9b8e4e16517104
Thierry Chappuis <tchappui@users.noreply.github.com>
2018-07-12 12:53:52 +0200

    Update publishconf.py

--- publishconf.py
+++ publishconf.py
@@ -10,7 +10,7 @@ import sys
 sys.path.append(os.curdir)
 from pelicanconf import *
 
-SITEURL = 'http://www.pythonclassmates.org'
+SITEURL = 'https://www.pythonclassmates.org'
 RELATIVE_URLS = False
 
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
