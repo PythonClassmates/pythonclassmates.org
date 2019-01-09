5b7f405277ba3aaab8b2483691711f23f9d7ce90
Thierry Chappuis <thierry@chappuis.io>
2019-01-09 18:20:43 +0100

    Added docstrings to the tasks.py functions

--- tasks.py
+++ tasks.py
@@ -14,6 +14,9 @@ PLUGINS_CMD = (
 )
 
 def check_theme_and_plugins(c):
+    """Checks if the theme and plugins directories are present and clones them
+    from Github if necessary.
+    """
     if not Path('./theme').exists():
         c.run(THEME_CMD)
     if not Path('./plugins').exists():
@@ -21,28 +24,37 @@ def check_theme_and_plugins(c):
 
 @task
 def build(c):
+    """Builds the local Pelican blog."""
     check_theme_and_plugins(c)
     c.run('echo "Publishing your Pelican website"')
     c.run(f'pipenv run pelican content -s pelicanconf.py -t {THEME}')
 
 @task
 def publish(c):
+    """Builds the Pelican blog with deployment settings."""
     check_theme_and_plugins(c)
     c.run('echo "Building your Pelican website"')
     c.run(f'pipenv run pelican content -s publishconf.py -t {THEME}')
 
 @task
 def autoreload(c):
+    """Starts the autoreload server to help during writing of blog articles."""
     c.run('echo "Running autoreload server. Press CTRL+C to stop"')
     c.run(f'pipenv run pelican -r content -s pelicanconf.py -t {THEME}')
 
 @task
 def runserver(c):
+    """Starts the dev server to visualize the articles locally in a web browser 
+    at url http://localhost:8000.
+    """
     c.run('echo "Running development server. Press CTRL+C to stop"')
     c.run(f'pipenv run python -m http.server -d output')
 
 @task
 def revert(c):
+    """Reverts the repository to the previous commit if not on a Pull Request
+    on Travis.
+    """
     if not os.getenv('TRAVIS_PULL_REQUEST'):
         c.run('echo "Build errors were encountered. Reverting last commit..."')
         c.run('git revert HEAD -n') 
